12c0bd8e826eceb96c280a14505f6970294e1b58
Washira <40855962+Washira@users.noreply.github.com>
2019-03-28 13:22:42 +0700

    Create cal_sum_avg_min_max_from_five_custom_numbers.py

---
+++ cal_sum_avg_min_max_from_five_custom_numbers.py
@@ -0,0 +1,33 @@
+number=[]
+x=5
+i=1
+while i <= x:
+  print('Number %d:' %i, end='')
+  n=int(input())
+  number.append(n)
+  i+=1
+Sum=0
+i=1
+while i <=x:
+  print(number[i-1], end = ', ')
+  Sum+= number[i-1]
+  i+=1
+Min=0
+Max=0
+i=2
+while i<x:
+  N=0
+  M=0
+  if number[i-2]>number[i-1]:
+    N=number[i-1]
+    M=number[i-2]
+  else:
+    N=number[i-2]
+    M=number[i-1]
+  Min=N
+  Max=M
+  i+=1
+print('\nSum = %d' %Sum)
+print('Average = %f' %(Sum/x))
+print('\nMin = %d' %Min)
+print('\nMax = %d' %Max)
